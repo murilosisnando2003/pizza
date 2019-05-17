1a4d2f0bc8bcca6402774008182a2d018eb7a833
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2019-05-16 22:28:10 -0300

    final

--- pizza/views.py
+++ pizza/views.py
@@ -8,6 +8,8 @@ def home(request):
 def order(request):
     if request.method=='POST':
         filled_form=PizzaForm(request.POST)
+        # filled_form=PizzaForm(request.POST,request.FILES)
+        
         if filled_form.is_valid():
             note = 'Thanks for you pizza!, Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
             filled_form.cleaned_data['topping1'],
