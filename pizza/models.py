6e321a72bd38f3c919f794d065e3c92d5360da7a
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2019-05-16 21:39:08 -0300

    Go

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    tittle = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tittle
+
+class Pizza(models.Model):
+    topping1 = models.CharField(max_length=100)
+    topping2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size,models.CASCADE)
