accabe702136e446d527aa62f5d5d1afeaa2bf37
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2019-05-19 20:08:29 -0300

    formschanges

--- pizza/forms.py
+++ pizza/forms.py
@@ -11,7 +11,12 @@ class PizzaForm(forms.ModelForm):
     # size = forms.ModelChoiceField(queryset=Size.objects,empty_label=None, widget=forms.CheckboxSelectMultiple)
     # image = forms.ImageField()
 
+    # email = forms.EmailField()
+
     class Meta:
         model = Pizza
         fields = ['topping1','topping2','size']
         labels = {'topping1': 'Topping 1','topping2': 'Topping 2'}
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2,max_value=6)
